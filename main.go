a2fc894fbc3d0782bc46285e36ca6efde6a90c58
Rohan Khatua <90958884+RohanKhatua@users.noreply.github.com>
2023-08-27 21:47:44 +0530

    feat: update routes

--- main.go
+++ main.go
@@ -27,6 +27,19 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/books", routes.GetBooks)
 	app.Get("/api/books/:id", routes.GetBookDetails)
 	app.Post("/api/books", routes.CreateBook)
+	app.Delete("/api/book/:id", routes.DeleteBook)
+
+	// cart routes
+
+	app.Get("/api/cart", routes.GetCartItems)
+	app.Post("/api/cart", routes.AddToCart)
+	app.Delete("/api/cart", routes.RemoveFromCart)
+
+	// Review routes
+
+	app.Post("/api/review", routes.AddReview)
+	app.Get("/api/review/:id", routes.GetBookReviews) //reviews for a book
+	app.Get("/api/review", routes.GetUserReviews) //reviews made by the user
 
 }
 
