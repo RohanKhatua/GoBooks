edaace4ec3b4b5bfdcae66634a12916e055c4827
Rohan Khatua <90958884+RohanKhatua@users.noreply.github.com>
2023-08-27 12:06:58 +0530

    build changes

--- main.go
+++ main.go
@@ -14,9 +14,20 @@ func welcome(c *fiber.Ctx) error {
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/api", welcome)
+
+	// user routes
 	app.Post("/api/signup", routes.SignUp)
+	app.Post("/api/login", routes.Login)
+
+	// implement middleware
 	app.Use(routes.JWTMiddleware)
 	app.Get("/api/protected", routes.ExampleProtectedRoute)
+
+	//book routes
+	app.Get("/api/books", routes.GetBooks)
+	app.Get("/api/books/:id", routes.GetBookDetails)
+	app.Post("/api/books", routes.CreateBook)
+
 }
 
 func main() {
