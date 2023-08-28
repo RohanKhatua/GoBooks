5f34899a5a80fa63813be1428504f0be32ca1170
Rohan Khatua <90958884+RohanKhatua@users.noreply.github.com>
2023-08-28 18:26:26 +0530

    feat: add clean DB and seed DB functionality

--- database/database.go
+++ database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"time"
+
 	"github.com/RohanKhatua/fiber-jwt/customLogger"
 	"github.com/RohanKhatua/fiber-jwt/models"
 	"gorm.io/driver/postgres"
@@ -45,4 +47,66 @@ func ConnectDb () {
 	
 	Database = DbInstance{Db: db}
 
+}
+
+func cleanDatabase (db *gorm.DB) {
+	db.Exec("DELETE FROM users")
+	db.Exec("DELETE FROM books")
+	db.Exec("DELETE FROM cart_items")
+	db.Exec("DELETE FROM purchases")
+	db.Exec("DELETE FROM reviews")	
+}
+
+func SeedDatabase (db *gorm.DB) {
+	// Clean database
+	cleanDatabase(db)
+
+	// Create sample users
+	users := []models.User{
+		{UserName: "user1", Password: "pass1", Role: "user", IsActivated: true},
+		{UserName: "user2", Password: "pass2", Role: "user", IsActivated: true},
+	}
+	for i := range users {
+		db.Create(&users[i])
+	}
+
+	// Create sample books
+	books := []models.Book{
+		{Author: "Author 1", Year: 2020, Title: "Book 1", Quantity: 10, Description: "Description 1", Price: 20},
+		{Author: "Author 2", Year: 2019, Title: "Book 2", Quantity: 5, Description: "Description 2", Price: 15},
+	}
+	for i := range books {
+		db.Create(&books[i])
+	}
+
+	// Create sample cart items
+	cartItems := []models.CartItem{
+		{UserID: users[0].ID, BookID: books[0].ID},
+		{UserID: users[1].ID, BookID: books[1].ID},
+	}
+	for i := range cartItems {
+		db.Create(&cartItems[i])
+	}
+
+	// Create sample purchases
+	purchases := []models.Purchase{
+		{UserID: int(users[0].ID), BookID: books[0].ID, Quantity: 2, PurchaseDate: time.Now()},
+		{UserID: int(users[1].ID), BookID: books[1].ID, Quantity: 1, PurchaseDate: time.Now()},
+	}
+	for i := range purchases {
+		db.Create(&purchases[i])
+	}
+
+	// Create sample reviews
+	reviews := []models.Review{
+		{UserID: users[0].ID, BookID: books[0].ID, Rating: 4, Comment: "Good book", ReviewDate: time.Now()},
+		{UserID: users[1].ID, BookID: books[1].ID, Rating: 5, Comment: "Excellent read", ReviewDate: time.Now()},
+	}
+	for i := range reviews {
+		db.Create(&reviews[i])
+	}
+
+	myLogger := customLogger.NewLogger()
+
+	myLogger.Info("Seeding Complete")
 }
