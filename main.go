deddcc26e0feab48a243cde0d94e2b97d125581f
Rohan Khatua <90958884+RohanKhatua@users.noreply.github.com>
2023-08-28 12:18:14 +0530

    feat: add routes to main

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func welcome(c *fiber.Ctx) error {
-	return c.SendString("Welcome to GoAuth")
+	return c.SendString("Welcome to the Bookstore")
 }
 
 func setupRoutes(app *fiber.App) {
@@ -23,16 +23,23 @@ func setupRoutes(app *fiber.App) {
 	app.Use(routes.JWTMiddleware)
 	app.Get("/api/protected", routes.ExampleProtectedRoute)
 
+	//user activation routes
+
+	app.Put("/api/activate", routes.ActivateUser)
+	app.Put("/api/deactivate", routes.DeactivateUser)
+
 	//book routes
-	app.Get("/api/books", routes.GetBooks)
-	app.Get("/api/books/:id", routes.GetBookDetails)
-	app.Post("/api/books", routes.CreateBook)
-	app.Delete("/api/book/:id", routes.DeleteBook)
+	app.Post("/api/book", routes.CreateBook)
+	app.Get("/api/book/:id", routes.GetBookDetails)
+	app.Get("/api/book", routes.GetBooks)
+	app.Delete("/api/book", routes.DeleteBook)
+	app.Put("/api/book", routes.UpdateBook)
+	app.Put("/api/book/quantity", routes.ChangeBookQuantity) //admin only route
 
 	// cart routes
 
-	app.Get("/api/cart", routes.GetCartItems)
 	app.Post("/api/cart", routes.AddToCart)
+	app.Get("/api/cart", routes.GetCartItems)
 	app.Delete("/api/cart", routes.RemoveFromCart)
 
 	// Review routes
@@ -40,6 +47,13 @@ func setupRoutes(app *fiber.App) {
 	app.Post("/api/review", routes.AddReview)
 	app.Get("/api/review/:id", routes.GetBookReviews) //reviews for a book
 	app.Get("/api/review", routes.GetUserReviews) //reviews made by the user
+	app.Put("/api/review", routes.EditReview)
+	app.Delete("/api/review", routes.DeleteReview)
+	app.Get("/api/rating/:id", routes.GetAverageRating)
+
+	// Purchase routes
+
+	app.Post("/api/purchase", routes.MakePurchase)
 
 }
 
